728f00f4df04a3d31cd596c01a38b23e6c471017
Noah A Klem <noahaklem@gmail.com>
2023-09-11 11:54:26 -0600

    School methods

--- lib/school.rb
+++ lib/school.rb
@@ -8,4 +8,25 @@ class School
         @school = name
         @roster = {}
     end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, student|
+            sorted_roster[grade] = student.sort
+        end 
+        sorted_roster
+    end
 end
